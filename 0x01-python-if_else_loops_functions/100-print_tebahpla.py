2ae3279347cd474a0702ee27d8b13f14926ffe90
Cynthia Dodzi <dahesey@gmail.com>
2023-10-11 22:23:15 +0000

    Added a space around an operator

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for alphabet in range(122, 96, -1):
     print("{:c}".format(alphabet if alphabet % 2 == 0
-          else alphabet -32), end="")
+          else alphabet - 32), end="")
