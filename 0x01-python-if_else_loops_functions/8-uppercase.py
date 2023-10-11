0229a1045697164dedceb22ed6501fd8f724895b
Cynthia Dodzi <dahesey@gmail.com>
2023-10-11 20:10:48 +0000

    Pycodestyle

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,6 +4,6 @@ islower = __import__('7-islower').islower
 
 def uppercase(str):
     for char in str:
-        print("{}".format(char if not islower(char) else chr(ord(char)-32)),\
+        print("{}".format(char if not islower(char) else chr(ord(char)-32)),
  end="")
     print(end="\n")
