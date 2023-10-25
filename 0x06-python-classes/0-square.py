fa519f3e10177eaa53be97fff0d6dc71085169d5
Dahesey <dahesey@gmail.com>
2023-10-25 15:33:48 -0700

    Added documentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
+"""define a class"""
 class Square:
+    """Define an empty class"""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""define A class"""
 class Square:
+    """initialise attributes with self"""
     def __init__(self, size):
+        """define a private instance attribute"""
         self.__size =size
