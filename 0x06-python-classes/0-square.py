e2b9c0f5594937dcd98dbc0029c4aece610828fe
Dahesey <dahesey@gmail.com>
2023-10-26 04:18:21 -0700

    Pycodestyle compliance

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """define a class"""
+
+
 class Square:
     """Define an empty class"""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """define A class"""
+
+
 class Square:
     """initialise attributes with self"""
     def __init__(self, size):
         """define a private instance attribute"""
-        self.__size =size
+        self.__size = size
