d80d69364ba62e7547af05d7aa619db3632111f7
Dahesey <dahesey@gmail.com>
2023-11-18 17:00:26 +0000

    Added a fullstop i missed. lol

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     import sys
     num_args = len(sys.argv) - 1
     if num_args == 0:
-        print("{:d} arguments".format(num_args))
+        print("{:d} arguments.".format(num_args))
     else:
         arg_str = "argument" if num_args == 1 else "arguments"
         i = 1
