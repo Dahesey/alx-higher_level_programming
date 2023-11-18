fc6bd841aae997132fc14e1013a5326907708387
Dahesey <dahesey@gmail.com>
2023-11-18 16:51:10 +0000

    To print number of arguments passed

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""printing argument count and the arguments with indexing"""
+
+if __name__ == '__main__':
+    import sys
+    num_args = len(sys.argv) - 1
+    if num_args == 0:
+        print("{:d} arguments".format(num_args))
+    else:
+        arg_str = "argument" if num_args == 1 else "arguments"
+        i = 1
+        print("{:d} {:s}".format(num_args, arg_str))
+        while i <= num_args:
+            print("{:d}: {:s}".format(i, sys.argv[i]))
+            i += 1
