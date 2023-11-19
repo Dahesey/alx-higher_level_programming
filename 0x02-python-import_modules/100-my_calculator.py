5e20fdacb45f1614ace45d847bbfd18c56b53481
Dahesey <dahesey@gmail.com>
2023-11-19 11:31:06 +0000

    corrected operator name

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -16,11 +16,11 @@ if __name__ == '__main__':
     a = int(sys.argv[1])
     b = int(sys.argv[3])
 
-    if operator == '+':
+    if opt == '+':
         print("{} + {} = {}".format(a, b, add(a, b)))
-    elif operator == '-':
+    elif opt == '-':
         print("{} - {} = {}".format(a, b, sub(a, b)))
-    elif operator == '*':
+    elif opt == '*':
         print("{} * {} = {}".format(a, b, mul(a, b)))
     else:
         print("{} / {} = {}".format(a, b, div(a, b)))
