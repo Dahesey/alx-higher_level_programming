ca5a29e19170b4908fdfa33fc68d17d93f23231f
Dahesey <dahesey@gmail.com>
2023-11-19 12:29:06 +0000

    Added space around the return keyword

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -3,7 +3,7 @@
 
 def magic_calculation(a, b):
     """match with holberton bytecode"""
-    from calculator_1 import add, sub
+    from magic_calculation_102 import add, sub
 
     if a < b:
         c = add(a, b)
