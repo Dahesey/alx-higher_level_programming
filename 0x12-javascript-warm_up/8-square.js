d9151a703c7b7cbddbd8bbe400faebf95a3615af
Dahesey <dahesey@gmail.com>
2024-01-10 13:37:57 +0000

    A script to print a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const x = parseInt(process.argv[2]);
+
+if (!isNaN(x)) {
+  for (let i = 0; i < x; i++) {
+    let square = '';
+    for (let n = 0; n < x; n++) {
+      square += 'X';
+    }
+    console.log(square);
+  }
+} else {
+  console.log('Missing Size');
+}
