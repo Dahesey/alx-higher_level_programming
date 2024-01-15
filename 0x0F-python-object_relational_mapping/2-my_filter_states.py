0b41c94a56698b4a94b933336d08c14f83b317bb
Dahesey <dahesey@gmail.com>
2024-01-15 10:04:47 +0000

    Added a multilevel comment

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -18,7 +18,8 @@ if __name__ == "__main__":
             state_name = sys.argv[4],
             charset='utf8')
     cursor = db.cursor()
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
+    query = """SELECT * FROM states WHERE name = '{}' 
+        ORDER BY id ASC""".format(state_name)
     cursor.execute(query)
 
     states = cursor.fetchall()
