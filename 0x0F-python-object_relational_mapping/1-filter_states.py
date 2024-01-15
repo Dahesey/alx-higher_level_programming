cf87fa27fa160a711186bc3f04c83d0a468b8444
Dahesey <dahesey@gmail.com>
2024-01-15 05:12:42 +0000

    Added a comma

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -17,8 +17,8 @@ if __name__ == "__main__":
             db=sys.argv[3],
             charset='utf8')
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC")
-    rows = cursors.fetchall()
+    cursor.execute("""SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC""")
+    rows = cursor.fetchall()
     for row in rows:
         print(row)
     cursor.close()
