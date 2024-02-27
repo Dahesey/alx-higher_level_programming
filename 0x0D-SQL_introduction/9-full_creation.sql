54e0ad0051c267aad4c1c773814b99c5b65fd4c5
Cynthia Dodzi <dahesey@gmail.com>
2024-02-27 10:17:08 +0000

    Added spaces

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,7 @@
 -- A script that creates a new table in the database
 
 CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
-	--inserting values into table
+	-- inserting values into table
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES(1, 'John', 10);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES(2, 'Alex', 3);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES(3, 'Bob', 14);
