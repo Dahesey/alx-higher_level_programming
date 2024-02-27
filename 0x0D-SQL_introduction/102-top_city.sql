ffacf58f09363d96544bb845b76bb848ad918776
Cynthia Dodzi <dahesey@gmail.com>
2024-02-27 09:38:04 +0000

    Added spaces in comment in all files

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,4 +1,4 @@
 -- A script that displays the top 3 of cities temparature
 -- during July and August ordered b temperature in descending order
 
-SELECT `city`, AVG(`value`) 'avg_temp' FROM `temperatures` WHERE `month` = 7 OR `month = 8 GROUP BY `city` ORDER BY `avg_temp` DESC LIMIT 3;
+SELECT `city`, AVG(`value`) `avg_temp` FROM `temperatures` WHERE `month` = 7 OR `month = 8 GROUP BY `city` ORDER BY `avg_temp` DESC LIMIT 3;
