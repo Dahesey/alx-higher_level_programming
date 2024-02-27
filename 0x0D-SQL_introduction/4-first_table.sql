84e53396f503c3d78a646bf2eebc5c13c4ae7f7d
Cynthia Dodzi <dahesey@gmail.com>
2024-02-27 10:05:01 +0000

    Added single quotes

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,3 @@
 -- A Script that creates a tabel called first_table
 
-CREATE TABLE IF NOT EXISTS first_table(
-	id INT
-	name VARCHAR(256)
-	);
+CREATE TABLE IF NOT EXISTS first_table(id INT, name VARCHAR(256));
