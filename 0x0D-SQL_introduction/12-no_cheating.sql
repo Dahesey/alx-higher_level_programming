75f18c37e9878ac8e84a294900f20fc77e9250a3
Cynthia Dodzi <dahesey@gmail.com>
2024-02-27 09:48:20 +0000

    addes a single quote

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,3 +1,3 @@
 -- A script that updates the score of Bob in the second_table
 
-UPDATE `second_table` SET `score` = 10 WHERE `name` = `Bob`;
+UPDATE `second_table` SET `score` = 10 WHERE `name` = 'Bob';
